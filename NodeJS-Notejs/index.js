4c7c5a3d7f700ca97334ed5527783d3977fa1bd5
jesielcs <81185418+jesielcs@users.noreply.github.com>
2021-03-22 15:19:55 -0300

    Add files via upload

---
+++ NodeJS-Notejs/index.js
@@ -0,0 +1,81 @@
+
+
+
+/*Aqui começa o código de verdade*/ 
+var express = require('express')
+var app = express()
+
+const path=require('path');
+const router = express.Router();
+_dirname=path.resolve();
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/',  (req, res) => {
+ 
+res.render(_dirname+'/view/home.html');
+})
+
+
+app.get('/operacao',  function(req,res)  {
+
+ let {numero1, numero2} = req.query;
+
+    var subtracao = numero1 - numero2;
+    
+     res.send("O resultado da subtração dos número é:"+subtracao);
+
+res.render(_dirname+'/view/home.html');
+})
+
+app.post('/operacao',  function(req,res)  {
+
+  var numero1=req.body.numer1;
+  var numero2=req.body.numer2;
+
+  var soma = parseInt(numero1) + parseInt(numero2);
+    
+     res.send("O resultado da soma dos número é:"+soma);
+
+res.render(_dirname+'/view/home.html');
+})
+
+
+app.get("/exercicio",  (req, res) => {
+ 
+res.render(_dirname+'/view/exercicio2.html');
+})
+
+app.post('/soma',  function(req,res)  {
+
+numeros = req.body
+    var preencherObjeto = Object.values(numeros).reduce( (itemA, itemB) => {
+      //A chamada é feita em recursão
+        numero1 = parseInt(itemA)
+        numero2 = parseInt(itemB)
+        return numero1 + numero2
+    })
+    
+    res.send("A soma dos campos é: " +preencherObjeto)
+  
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000!');
+
